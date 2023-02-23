9b089618292b928d8089b638f61509c9a2edec9e
Intern IOS <ios@Interns-MacBook-Air.local>
2023-02-23 15:33:01 +0530

    added

---
+++ sample project/sample project/sample_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_projectApp.swift
+//  sample project
+//
+//  Created by Intern IOS on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct sample_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
